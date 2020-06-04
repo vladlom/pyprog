9ca56d5f0bcee361dd7d86d6f2bc6b97901418b3
vladlom <ios561@yandex.ru>
2020-06-04 18:06:32 +0300

    upgrade code in ploshad.py

--- ploshad.py
+++ ploshad.py
@@ -1,11 +1,11 @@
 cikl = 1
-count = 1
+count = 0
 checkNo = ['No','no','Нет','нет']
 checkYes = ['Yes','yes','Да','да']
 checkYesNo = checkNo + checkYes
 
 
-def checkFloat():
+def checkFloat(count):
     checkCicle = 0
     while checkCicle == 0:
         try:
@@ -14,29 +14,44 @@ def checkFloat():
         except:
             print('Вы ввели не число, повторите ввод!')
             checkCicle =0
-    return floAt
+            count = count - 1
+    return floAt, count
 
 
 while cikl == 1:
     cicleCount = 0
     while cicleCount == 0:
-        YesNo = input('\n Желаете продолжить? Введите <Да или Нет>  >>>   ')
-        if YesNo in checkYesNo:
+        count = count + 1
+        if count == 1:
+            print('Добро пожаловать в программу вычисления площади окна. =)')
+            YesNo = 'Yes'
             cicleCount = 1
         else:
-            print('\n Вы ввели некорректный ответ. Повторите пожалуйста ввод!')
-            cicleCount = 0
+            YesNo = input('Желаете продолжить? Введите <Да или Нет>  >>>   ')
+            if YesNo in checkYesNo:
+                cicleCount = 1
+            else:
+                print('Вы ввели некорректный ответ. Повторите пожалуйста ввод!')
+                cicleCount = 0
+                count = count - 1
     if YesNo in checkYes:
-        print('Введите высоту окна. ', end=' ')
-        windowHeight = checkFloat()
-        print('Введите ширину окна. ', end=' ')
-        windowWidth = checkFloat()
-        windowArea = windowHeight * windowWidth
-        print('Площадь окна равна', windowArea)
-        strWindowHeight = '  Высота окна  ' + str(windowHeight) + '\n'
-        strWindowWidth = '  Ширина окна  ' + str(windowWidth) + '\n'
-        strWindowArea = '  Площадь окна  ' + str(windowArea) + '\n'
-        fileOpen = open('area.txt', 'w')
+        strCount = str(count)
+        print('Введите высоту ' + strCount + '-го окна. ', end=' ')
+        windowHeight = checkFloat(count)
+        print('Введите ширину ' + strCount + '-го окна. ', end=' ')
+        windowWidth = checkFloat(count)
+        windowArea = windowHeight[0] * windowWidth[0]
+        print('Площадь ' + strCount + '-го окна равна', windowArea)
+        strWindowHeight = 'Высота ' + strCount + '-го окна =' + \
+                str(windowHeight[0]) + '\n'
+        strWindowWidth = 'Ширина ' + strCount + '-го окна =' + \
+                str(windowWidth[0]) + '\n'
+        strWindowArea = 'Площадь ' + strCount + '-го окна =' + \
+                str(windowArea) + '\n\n'
+        if count == 1:
+            fileOpen = open('area.txt', 'w')
+        else:
+            fileOpen = open('area.txt', 'a')
         fileOpen.write(strWindowHeight)
         fileOpen.write(strWindowWidth)
         fileOpen.write(strWindowArea)
@@ -45,5 +60,3 @@ while cikl == 1:
         print('-= Конец прогрвммы, выход. =-')
         cikl = 0
         break
-#        ciklChek = ciklChek + 1
-
