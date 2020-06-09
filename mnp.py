7a9bd8b326b86a62337351f05b331d9dae9450df
vladlom <ios561@yandex.ru>
2020-06-09 16:09:21 +0300

    upgrade code in mnp.py

--- mnp.py
+++ mnp.py
@@ -1,11 +1,40 @@
 from datetime import datetime
+
+
 areaBody = open('area_body.db', 'r')
 areaBodyR = areaBody.read()
 areaBodyR = areaBodyR.rstrip()
 areaBody.close()
+
+
 dateName = str(datetime.now().date())
 dateName = dateName.replace('-','')
 projectName = dateName + areaBodyR
+
+
 fileOpen = open(projectName + '.db', 'w')
-fileOpen.write(projectName)
+fileOpen.write('project No ' + projectName + '\n')
+name = input('Please enter nama of the customer >>> ')
+surname = input('Please enter surname of the customer >>> ')
+address = input('Please enter address of the customer >>> ')
+phone = input ('Please enter phone of the customer >>> ')
+fileOpen.write('Name - ' + name + '\n')
+fileOpen.write('Surname - ' + surname + '\n')
+fileOpen.write('Address - ' + address + '\n')
+fileOpen.write('Phone number - ' + phone + '\n')
+
+
 fileOpen.close()
+
+
+
+
+areaBodyR = int(areaBodyR)
+areaBodyR += 1
+areaBodyR = str(areaBodyR)
+
+
+areaBody = open('area_body.db', 'w')
+areaBody.write(areaBodyR)
+areaBody.close()
+
